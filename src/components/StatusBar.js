d2881e151f05db9ce089426f0b3a52cafcdd3ebb
Diego Luis <contato.diegoluisdasilva@gmail.com>
2021-05-04 23:55:45 -0300

    Criação dos Assets

--- src/components/StatusBar.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import colors from '../styles/colors';
-
-export const StatusBar = styled.StatusBar.attrs({
-  barStyle: 'dark-content',
-  backgroundColor: colors.primary,
-})``;
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/',
-});
-
-export default api;
-
-//fazer uma função que pega os dados das apis, junta e retorna o necessário, um arquivo apiHooks com esta função
